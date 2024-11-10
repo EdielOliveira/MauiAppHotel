7c1217b9b9c6b8c8b8c4971ef0372d394f49ef89
EdielOliveira <ediel.inacio@outlook.com>
2024-11-10 19:20:17 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/Views/SobrePage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.Maui.Controls;
+
+namespace MauiAppHotel.Views
+{
+    public partial class SobrePage : ContentPage
+    {
+        public SobrePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnVoltarClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync(); // Volta para a página anterior
+        }
+    }
+}
